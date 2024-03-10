26f6d008c52040d0c74a65d8f6f5c028cf80adf1
Antoine <75070662+anbotz@users.noreply.github.com>
2024-03-10 22:43:18 +0100

    build(app): avoid build error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,53 +1,6 @@
-import { db } from "@/lib/db";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import type { Adapter } from "next-auth/adapters";
-import { getPermissions } from "@/lib/permission";
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth-options";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(db) as Adapter,
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-  callbacks: {
-    // FIXME : remove jwt ???
-    async jwt({ token }) {
-      const dbUser = await db.user.findUnique({
-        where: {
-          email: token.email!,
-        },
-      });
-
-      const permissions = getPermissions(dbUser?.role);
-
-      token.permissions = permissions;
-      token.id = dbUser?.id;
-
-      return token;
-    },
-    async session({ token, session }) {
-      if (token && session.user) {
-        session.user.permissions = token.permissions;
-        session.user.id = token.id;
-      }
-
-      return session;
-    },
-  },
-};
-
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- src/app/batch/page.tsx
+++ src/app/batch/page.tsx
@@ -2,9 +2,9 @@ import { BatchButtons } from "@/_components/buttons/batch-buttons";
 import BatchGrid from "@/_components/container/batch-grid";
 import ProtectedContentContainer from "@/_components/container/protected-content-container";
 import { PageLayoutComponent } from "@/_components/layout/page-layout";
+import { authOptions } from "@/lib/auth-options";
 import batchService from "@/lib/batch/service";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 
 const days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi"];
 
