240678725e9630671c83abfac9ef944d28bd8410
Antoine <75070662+anbotz@users.noreply.github.com>
2024-05-02 11:05:57 +0200

    feat(ai): add model to env

--- src/lib/ai/ai.client.ts
+++ src/lib/ai/ai.client.ts
@@ -11,7 +11,9 @@ const groqClient = new Groq({
 });
 
 const openAiConfig: AiConfig = { model: "gpt-3.5-turbo" };
-const groqConfig: AiConfig = { model: "llama2-70b-4096" };
+const groqConfig: AiConfig = {
+  model: process.env.GROQ_MODEL ?? "Llama3-8b-8192",
+};
 
 export const AIclient = groqClient;
 export const AiClientConfig: AiConfig = groqConfig;
